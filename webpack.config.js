d895dcbbf8c12591e6602aa98ea41da65c236e12
haku-noir <hakunoir.00@gmail.com>
2019-07-06 22:04:33 +0900

    chore: created-react-app--typescript を利用した雛形の作成
    git clone https://github.com/haku-noir/created-react-app--typescript.git を実行し、README.md 以外をコピー＆ペースト
    package.json は npm init -y で作成した package.json とマージ
    created-react-app/README.md の通りに雛形を作成

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack');
+const path = require('path');
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = [{
+  cache: true,
+  devtool: 'source-map',
+  devServer: {
+    historyApiFallback: true,
+  },
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.tsx',
+  output: {
+    path: __dirname + "/dist",
+    filename: 'build.js',
+    publicPath: '/dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+    ]
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, 'src'),
+      'node_modules'
+    ],
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './index.html',
+    })
+  ]
+}];
